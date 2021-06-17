7050e330f6bb9c8914f4b67cc2f53e76e232f573
abert <1248824453@qq.com>
2021-06-17 12:05:36 +0800

    feat: 新增浏览器的缓存方法storage

--- packages/tool/src/index.js
+++ packages/tool/src/index.js
@@ -3,15 +3,30 @@
 import is from './is'
 // import type from './type'
 import cache from './cache'
+import { LocalStorage, StorageFactory, Storage, SessionStorage } from './storage'
+import getQueryParams from './get_query_params'
+import buildQueryParams from './build_query_params'
 
 export default {
   is,
   // type,
-  cache
+  cache,
+  LocalStorage,
+  StorageFactory,
+  Storage,
+  SessionStorage,
+  getQueryParams,
+  buildQueryParams
 }
 
 export {
   is,
   // type,
-  cache
+  cache,
+  LocalStorage,
+  StorageFactory,
+  Storage,
+  SessionStorage,
+  getQueryParams,
+  buildQueryParams
 }
---
+++ packages/tool/src/storage.js
@@ -0,0 +1,53 @@
+'use strict'
+
+// import _ from 'lodash'
+
+const __NAME__ = 'STATION'
+const __MODULE_PREFIX_NAME__ = '_EHOME-COMMON_'
+
+// 本地缓存的工厂模式
+class StorageFactory {
+  constructor (prefix, target) {
+    this.prefix = __NAME__ + `_${prefix}`
+    this.target = target
+  }
+
+  set (key, data) {
+    try {
+      this.target.setItem(`${this.prefix}${key}`, JSON.stringify(data))
+    } catch (error) {
+      console.warn('storage set error', error)
+    }
+  }
+
+  get (key) {
+    const value = this.target.getItem(`${this.prefix}${key}`)
+    try {
+      return value ? JSON.parse(value) : value
+    } catch (error) {
+      console.error('storage get error', error)
+      return null
+    }
+  }
+
+  remove (key) {
+    this.target.removeItem(`${this.prefix}${key}`)
+  }
+
+  clear () {
+    this.target.clear()
+  }
+
+  // TODO 获取所有缓存 待实现
+  getAll () {}
+}
+
+const LocalStorage = new StorageFactory(__MODULE_PREFIX_NAME__, window.LocalStorage)
+
+const SessionStorage = new StorageFactory(__MODULE_PREFIX_NAME__, window.sessionStorage)
+
+const Storage = LocalStorage
+
+export { StorageFactory, Storage, LocalStorage, SessionStorage }
+
+export default Storage
