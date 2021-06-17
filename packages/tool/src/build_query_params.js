27fd9339ed01384d69051944bfb7751b66bfe413
abert <1248824453@qq.com>
2021-06-17 11:43:50 +0800

    fix: 修复buildQueryParam的实现方式

--- packages/tool/src/build_query_params.js
+++ packages/tool/src/build_query_params.js
@@ -6,15 +6,15 @@
  * @returns {string} (eg: ?a=1&b=1)
  */
 const buildQueryParams = (params = {}) => {
-  if (Object.prototype.toString(params) !== '[object Object]') {
+  if (Object.prototype.toString.call(params) !== '[object Object]') {
     console.warn('params must be an object !')
     throw new Error('params must be an object !')
   }
   const search = Object.entries(params).reduce((prev, [key, value], index) => {
     const cur = key + '=' + encodeURIComponent(value)
-    return index === 0 ? prev + cur : prev + '&' + cur
-  }, '?')
-  return search
+    return prev + '&' + cur
+  }, '')
+  return search.replace(/&/, '?')
 }
 
 export default buildQueryParams
